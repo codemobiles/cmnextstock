e509cba9cadc266db8f25ed0673005d706adf0d1
codemobiles <chaiyasit.t@gmail.com>
2020-06-02 16:02:25 +0700

    Ep4 - Custom Document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,14 +10,6 @@ function CMApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>CMNextStock</title>
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-        />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/icon?family=Material+Icons"
-        />
       </Head>
       <Component {...pageProps} />
     </>
---
+++ pages/_document.tsx
@@ -0,0 +1,29 @@
+import Document, { Head, Main, NextScript } from "next/document";
+
+export default class CMDoc extends Document {
+  render() {
+    return (
+      <html>
+        <Head>
+          <meta charSet="utf-8" />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
