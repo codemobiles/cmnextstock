05889e84d8a12f1c4cdcae4083262fd860dc2b0a
codemobiles <chaiyasit.t@gmail.com>
2020-06-03 11:03:03 +0700

    Ep9 - Redirect Page when Page Not found in ClientSide

--- pages/_error.tsx
+++ pages/_error.tsx
@@ -1,25 +1,31 @@
 import React, { ReactElement } from "react";
+import Router from "next/router";
 
 interface Props {
   statusCode: string;
 }
 
+// Called in client-side
 export default function Error({ statusCode }: Props): ReactElement {
-  return (
-    <div>
-      <h1>Error {statusCode}</h1>
-    </div>
-  );
+  React.useEffect(() => {
+    setTimeout(() => {
+      Router.push("/");
+    }, 2000);
+  }, []);
+
+  return <div>Error : {statusCode}</div>;
 }
 
 // Called in server-side
 Error.getInitialProps = ({ res, err }) => {
   const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
-  if (statusCode == 404) {
-    res.writeHead(301, { Location: "/" });
-    res.end();
-    return {};
-  } else {
-    return { statusCode };
-  }
+  return { statusCode };
+
+  //   if (statusCode == 404) {
+  //     res.writeHead(301, { Location: "/" });
+  //     res.end();
+  //     return {};
+  //   } else {
+  //     return { statusCode };
+  //   }
 };
