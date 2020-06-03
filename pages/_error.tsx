53ce02b8894797e1bc338cc553a1ee24eb0b3c42
codemobiles <chaiyasit.t@gmail.com>
2020-06-03 10:40:41 +0700

    Ep7 - Custom Error Page

---
+++ pages/_error.tsx
@@ -0,0 +1,19 @@
+import React, { ReactElement } from "react";
+
+interface Props {
+  statusCode: string;
+}
+
+export default function Error({ statusCode }: Props): ReactElement {
+  return (
+    <div>
+      <h1>Error {statusCode}</h1>
+    </div>
+  );
+}
+
+// Called in server-side
+Error.getInitialProps = ({ res, err }) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+  return { statusCode };
+};
