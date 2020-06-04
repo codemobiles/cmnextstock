f7af7b078c84f630b0666be2f12b587da58d2aed
codemobiles <chaiyasit.t@gmail.com>
2020-06-04 13:50:06 +0700

    Ep13 - CSS Module with JSS and CSS

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,16 +1,10 @@
 import React, { ReactElement } from "react";
 import { makeStyles, createStyles } from "@material-ui/core";
-
+import loginStyle from "../styles/login.style";
+import loginCSS from "../public/static/css/login.module.css";
 interface Props {}
 
-const styles = createStyles({
-  section: {
-    color: "red",
-    fontSize: 100,
-  },
-});
-
-const useStyles = makeStyles(styles);
+const useStyles = makeStyles(loginStyle);
 
 export default function Login({}: Props): ReactElement {
   const classes = useStyles();
@@ -18,7 +12,9 @@ export default function Login({}: Props): ReactElement {
   return (
     <div>
       <h1>Login</h1>
-      <div className={classes.section}>test</div>
+      <div className={classes.title}>Title</div>
+      <div className={classes.subtitle}>subtitle</div>
+      <div className={loginCSS.section}>subtitle</div>
     </div>
   );
 }
---
+++ styles/login.style.js
@@ -0,0 +1,14 @@
+import { createStyles } from "@material-ui/core";
+
+export default createStyles({
+  section: {
+    color: "green",
+    fontSize: 50,
+  },
+  title: {
+    fontWeight: 900,
+  },
+  subtitle: {
+    color: "gray",
+  },
+});
