5635c776267eb889e9f141fde72d2b9fcf8a942c
codemobiles <chaiyasit.t@gmail.com>
2020-06-05 10:14:42 +0700

    Ep15 - Login 1

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,20 +1,72 @@
 import React, { ReactElement } from "react";
-import { makeStyles, createStyles } from "@material-ui/core";
 import loginStyle from "../styles/login.style";
 import loginCSS from "../public/static/css/login.module.css";
+
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
 interface Props {}
 
-const useStyles = makeStyles(loginStyle);
-
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
 export default function Login({}: Props): ReactElement {
   const classes = useStyles();
 
   return (
-    <div>
-      <h1>Login</h1>
-      <div className={classes.title}>Title</div>
-      <div className={classes.subtitle}>subtitle</div>
-      <div className={loginCSS.section}>subtitle</div>
-    </div>
+    <React.Fragment>
+      <div>
+        <Card className={classes.root}>
+          <CardActionArea>
+            <CardMedia
+              className={classes.media}
+              image="/static/img/next_login.jpg"
+              title="Contemplative Reptile"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                Lizard
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                Lizards are a widespread group of squamate reptiles, with over
+                6,000 species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" color="primary">
+              Share
+            </Button>
+            <Button size="small" color="primary">
+              Learn More
+            </Button>
+          </CardActions>
+        </Card>
+
+        <style jsx global>
+          {`
+            body {
+              min-height: 100vh;
+              position: relative;
+              margin: 0;
+              background-size: cover;
+              background-image: url("/static/img/bg4.jpg");
+              text-align: center;
+            }
+          `}
+        </style>
+      </div>
+    </React.Fragment>
   );
 }
