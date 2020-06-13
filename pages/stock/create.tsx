853f31bc62b7e73f1be51b3ff04a8dfbbae8b62e
codemobiles <chaiyasit.t@gmail.com>
2020-06-13 14:14:20 +0700

    Ep35 - Edit Revised

--- pages/stock/create.tsx
+++ pages/stock/create.tsx
@@ -1,7 +1,35 @@
 import React, { ReactElement } from "react";
+import Header from "../../components/layouts/header";
+import Menu from "../../components/layouts/menu";
+import Layout from "../../components/layouts/layout";
+import MaterialTable, { Action } from "material-table";
+import { products } from "../api/dummy";
+import { Typography, makeStyles } from "@material-ui/core";
+import Moment from "react-moment";
+import NumberFormat from "react-number-format";
+import { Edit, DeleteOutline } from "@material-ui/icons";
+import { Formik, Form, Field } from "formik";
+import { TextField } from "formik-material-ui";
+import Container from "@material-ui/core/Container";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "80%",
+  },
+  field: {
+    marginTop: 16,
+  },
+  card: {
+    padding: 20,
+  },
+}));
 
 interface Props {}
 
 export default function StockCreate({}: Props): ReactElement {
-  return <div>Create</div>;
+  return <Layout>Create</Layout>;
 }
