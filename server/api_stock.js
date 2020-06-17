af5711f06762ce2c54eb416820c5940f07d14650
codemobiles <chaiyasit.t@gmail.com>
2020-06-17 15:42:50 +0700

    Ep53 - Backend with NodeJS Install DB Lib

--- server/api_stock.js
+++ server/api_stock.js
@@ -1,6 +1,12 @@
 const express = require("express");
-const products = require("./dummy");
 const router = express.Router();
+const product = require("./models/product");
+const Sequelize = require("sequelize");
+const constants = require("./constant");
+const formidable = require("formidable");
+const path = require("path");
+const fs = require("fs-extra");
+const Op = Sequelize.Op;
 
 router.get("/product", (req, res) => {
   res.json(products);
