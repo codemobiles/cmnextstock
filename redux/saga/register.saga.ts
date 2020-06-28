0d18b7c53c609dc00163f144bf17a22359c48d18
codemobiles <chaiyasit.t@gmail.com>
2020-06-28 20:18:03 +0700

    CMNextStock Ep72 Test Register Saga + Axios

--- redux/saga/register.saga.ts
+++ redux/saga/register.saga.ts
@@ -1,11 +1,21 @@
-import { put } from 'redux-saga/effects';
+import { put, call } from 'redux-saga/effects';
 import actions from "../actions"
+import httpClient from "../../utils/httpClient"
+import { kResultOk, kResultNok } from './../../utils/contants';
+import Router from 'next/router';
 
 
 export function* sagaRegister({ payload }: any) {
     try {
         yield put(actions.registerFetching())
-        yield put(actions.registerSuccess({ result: "ok" }))
+        const response = yield call(httpClient.post, '/authen/register', payload)
+        const { result } = response.data
+        if (result == kResultOk) {
+            yield put(actions.registerSuccess({ result: response.data }))
+            Router.push('/login')
+        } else {
+            yield put(actions.registerFailed())
+        }
     } catch (error) {
         yield put(actions.registerFailed())
     }
