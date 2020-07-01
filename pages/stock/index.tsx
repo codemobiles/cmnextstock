8eba2176db265be87f191eff9d6de9834cda3903
codemobiles <chaiyasit.t@gmail.com>
2020-07-01 13:51:37 +0700

    CMNextStock Ep91 Review Stock Part 12

--- pages/stock/index.tsx
+++ pages/stock/index.tsx
@@ -13,9 +13,25 @@ import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import stockActions from "../../redux/actions";
 
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+
 interface Props {}
 
 export default function Stock({}: Props): ReactElement {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   const columns = [
     {
       title: "ID",
@@ -133,6 +149,34 @@ export default function Stock({}: Props): ReactElement {
           ),
         }}
       />
+
+      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+        Open alert dialog
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Use Google's location service?"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Let Google help apps determine location. This means sending
+            anonymous location data to Google, even when no apps are running.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Disagree
+          </Button>
+          <Button onClick={handleClose} color="primary" autoFocus>
+            Agree
+          </Button>
+        </DialogActions>
+      </Dialog>
     </Layout>
   );
 }
