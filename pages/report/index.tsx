13f4f426a0f2f5ef402ffb6926d994692d7dda42
codemobiles <chaiyasit.t@gmail.com>
2020-07-05 11:26:19 +0700

    CMNextStock Ep96 ChartJS #2 - Use Demo Chart and Data

--- pages/report/index.tsx
+++ pages/report/index.tsx
@@ -15,6 +15,7 @@ import { Formik, Form, Field } from "formik";
 import { useDispatch, useSelector } from "react-redux";
 import actions from "../../redux/actions";
 import Router from "next/router";
+import { Line, Bar, Pie } from "react-chartjs-2";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -30,6 +31,33 @@ const useStyles = makeStyles((theme) => ({
 
 interface Props {}
 
+const data = {
+  labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+  datasets: [
+    {
+      label: "# of Votes",
+      data: [15, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        "rgba(255, 99, 132, 0.2)",
+        "rgba(54, 162, 235, 0.2)",
+        "rgba(255, 206, 86, 0.2)",
+        "rgba(75, 192, 192, 0.2)",
+        "rgba(153, 102, 255, 0.2)",
+        "rgba(255, 159, 64, 0.2)",
+      ],
+      borderColor: [
+        "rgba(255, 99, 132, 1)",
+        "rgba(54, 162, 235, 1)",
+        "rgba(255, 206, 86, 1)",
+        "rgba(75, 192, 192, 1)",
+        "rgba(153, 102, 255, 1)",
+        "rgba(255, 159, 64, 1)",
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+
 export default function Report({}: Props): ReactElement {
   const classes = useStyles();
   const stockCreateReducer = useSelector((state) => state.stockCreateReducer);
@@ -37,7 +65,31 @@ export default function Report({}: Props): ReactElement {
 
   return (
     <Layout>
-      <h1>Report</h1>
+      <div style={{ height: 300 }}>
+        <Line
+          data={data}
+          width={100}
+          height={100}
+          options={{ maintainAspectRatio: false }}
+        />
+      </div>
+      <div style={{ height: 300 }}>
+        <Bar
+          data={data}
+          width={100}
+          height={100}
+          options={{ maintainAspectRatio: false }}
+        />
+      </div>
+
+      <div style={{ height: 300 }}>
+        <Pie
+          data={data}
+          width={100}
+          height={100}
+          options={{ maintainAspectRatio: false }}
+        />
+      </div>
     </Layout>
   );
 }
